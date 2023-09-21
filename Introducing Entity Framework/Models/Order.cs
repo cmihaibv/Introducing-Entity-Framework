191b7ccadf856bbe55f9d9330fe96a54255ba11d
Mihai <cotimihai20@gmail.com>
2023-09-21 16:02:16 +0100

    Created the Order entity, contains informations about the order status, contents of the order and customer id

---
+++ Introducing Entity Framework/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace IntroducingEntityFramework.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderPlaced { get; set; }
+
+        public DateTime? OrderFulfilled { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; } = null!;
+
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!; 
+    }
+}
