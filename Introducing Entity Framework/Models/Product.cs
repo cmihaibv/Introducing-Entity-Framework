bf8e6670107d2dd0c314d4669b41ca58f9747d72
Mihai <cotimihai20@gmail.com>
2023-09-21 15:31:22 +0100

    Created the first entity named Product, this is used to define diferent products that can be bought from the store

--- Introducing Entity Framework/Models/Product.cs
+++ Introducing Entity Framework/Models/Product.cs
@@ -3,7 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IntroducingEntityFramework.Models
 {
-    internal class Product
+    public class Product
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        [Column(TypeName = "decimal(6 , 2)")]
+        public decimal Price { get; set; }
+
     }
 }
