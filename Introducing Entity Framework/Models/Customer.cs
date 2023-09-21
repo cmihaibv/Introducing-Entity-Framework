e60b89050b2b6b864e9acce24ac20c34336b984f
Mihai <cotimihai20@gmail.com>
2023-09-21 15:52:42 +0100

    Created the entity Customer, defines the data collected about the customer.

---
+++ Introducing Entity Framework/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroducingEntityFramework.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string? Address { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = null!;
+    }
+}
