9885829b78111f288c572afecd39007c92534e82
Mihai <cotimihai20@gmail.com>
2023-09-21 15:22:15 +0100

    Updated Project Name, removed underscore between words.

---
+++ Introducing Entity Framework/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntroducingEntityFramework.Models
+{
+    internal class Product
+    {
+    }
+}
