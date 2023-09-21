48ef0fbcea435c7c56be0bbefddc08f592cf7411
Mihai <cotimihai20@gmail.com>
2023-09-21 20:11:44 +0100

    Added database context class and set a connection string to an existing emtpy database(hardcoded - bad practice !)

---
+++ Introducing Entity Framework/Data/IntroducingEntityFrameworkContext.cs
@@ -0,0 +1,22 @@
+﻿using IntroducingEntityFramework.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroducingEntityFramework.Data;
+
+public class IntroducingEntityFrameworkContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+
+    public DbSet<Order> Orders { get; set; } = null!;
+
+    public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IntroducingEntityFrameworkDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0");
+    }
+
+}
