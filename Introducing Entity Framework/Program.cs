343ccea08a27108c5c9043d258e949d09e6f65e9
Mihai <cotimihai20@gmail.com>
2023-09-25 14:02:07 +0100

    Created a function to add products to the database

--- Introducing Entity Framework/Program.cs
+++ Introducing Entity Framework/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IntroducingEntityFramework.Data;
+using IntroducingEntityFramework.Models;
+
+using IntroducingEntityFrameworkContext context = new IntroducingEntityFrameworkContext();
+
+
+//Use this function to add a new item into the Products table.
+void AddItemToDatabase(String productName, decimal productPrice)
+{
+    
+    Product menuItem = new Product()
+    {
+        Name = productName,
+        Price = productPrice
+    };
+    context.Products.Add(menuItem);
+}
+
+AddItemToDatabase("Bolognese Spaghetti", 9.99M);
+AddItemToDatabase("Pizza Margherita", 12.99M);
+
+context.SaveChanges();
