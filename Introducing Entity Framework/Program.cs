d2b64b05c6a9af2969ca1757d56f860a484b2eb4
Mihai <cotimihai20@gmail.com>
2023-09-26 12:44:56 +0100

    Added function to search and display product from Products table, using LINQ.

--- Introducing Entity Framework/Program.cs
+++ Introducing Entity Framework/Program.cs
@@ -7,7 +7,7 @@ using IntroducingEntityFrameworkContext context = new IntroducingEntityFramework
 //Use this function to add a new item into the Products table.
 void AddItemToDatabase(String productName, decimal productPrice)
 {
-    
+
     Product menuItem = new Product()
     {
         Name = productName,
@@ -16,7 +16,22 @@ void AddItemToDatabase(String productName, decimal productPrice)
     context.Products.Add(menuItem);
 }
 
-AddItemToDatabase("Bolognese Spaghetti", 9.99M);
-AddItemToDatabase("Pizza Margherita", 12.99M);
+//AddItemToDatabase("Bolognese Spaghetti", 9.99M);
+//AddItemToDatabase("Pizza Margherita", 12.99M);
 
-context.SaveChanges();
+//context.SaveChanges();
+
+//Search method (looking for items above 10.00 in price) using LINQ
+//var products = from product in context.Products
+//               where product.Price >10.00M
+//               orderby product.Name
+//               select product;
+
+//foreach(Product p in products)
+//{
+//    Console.WriteLine($"ID:     {p.Id}");
+//    Console.WriteLine($"Name:   {p.Name}");
+//    Console.WriteLine($"Price:  {p.Price}");
+//    Console.WriteLine(new string('-', 20));
+//}
+
